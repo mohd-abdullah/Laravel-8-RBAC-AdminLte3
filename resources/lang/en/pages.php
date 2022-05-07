2c8e2cefbf60c67c381fded34d628fbab4cf8cdb
abdullah <mohdabdullah.it@gmail.com>
2022-05-07 14:07:55 +0530

    initial commit

---
+++ resources/lang/en/pages.php
@@ -0,0 +1,61 @@
+<?php
+return [
+    'region' => 'Region',
+    'district' => 'Street',
+    'fields' => [
+        'id' => '',
+        'name' => 'Name',
+        'merchant' => 'Merchant',
+        'terminal' => 'terminal',
+        'percentage' => 'Percentage',
+        'period' => 'Period',
+        'street' => 'Street',
+        'city' => 'City',
+        'district' => 'District',
+        'phone' => 'Number',
+        'phone2' => 'Number2',
+        'region' => 'Region',
+        'address' => 'Address',
+        'client_name' => 'name',
+        'loan_id' => 'Loan#',
+        'summ_claim' => 'Amount',
+        'brand_id' => 'Product',
+        'type' => 'Merchandise type'
+    ],
+    'merchant' => [
+        'config' => 'Merchant configurations',
+        'title' => 'Merchant Management',
+        'title_singular' => 'Merchant',
+    ],
+    'brand' => [
+        'title' => 'Merchandise Management',
+        'title_singular' => 'Merchandise'
+    ],
+    'client' => [
+        'title' => 'Customer Management',
+        'title_singular' => 'Clients',
+        'info' => 'Information',
+        'id' => 'identifier',
+        'client_id' => 'ClientID',
+        'branch_code' => 'Branch Code',
+        'summ_claim' => 'Request amount',
+        'claim_date' => 'Request date',
+        'close_date' => 'Close date',
+        'client_name' => 'Client name',
+        'birth_date' => 'Birthday',
+        'phone' => 'Phone',
+        'phone2' => 'Phone2',
+        'doc_number' => 'Document number',
+        'inn' => 'inn',
+        'doc_reg_date' => 'Document registration date',
+        'dog_reg_place' => 'Document registration location',
+        'region_code' => 'Region',
+        'district_code' => 'District',
+        'address' => 'Address',
+        'loan_id' => 'Debt',
+        'condition' => 'Condition',
+    ],
+]
+
+
+?>
---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'password' => 'The password must be at least 6 characters long and must match the confirmation',
+    'reset' => 'Your password has been reset',
+    'sent' => "We've sent you almost a link to reset your password",
+    'token' => 'The password reset key is incorrect',
+    'user' => 'We can\'t find a user with this email address',
+    'updated' => 'Your password has been changed!'
+];
